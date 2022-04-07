e0acd9e30a7b8e7056302a9cd48b408c3880bffb
William Chrisandy <william_chrisandy@yahoo.com>
2022-04-07 12:35:04 +0700

    - Test

--- SleepMuse/ViewController.swift
+++ SleepMuse/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //JESI
-        
+        //TEST YEYYY
+        super.viewDidLoad() //DELETE AJAA
     }
 
 
